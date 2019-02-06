27017247bc0cb615247e14050d29fa0573380b36
Yankı Ekin Yüksel <yanechednezzar@gmail.com>
2019-02-07 01:08:30 +0300

    clean again

--- sw.js
+++ sw.js
@@ -2,28 +2,28 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/11848600b7d4aec12a5a.js",
-    "revision": "b40319d444e9ccb320f7001630a3238c"
+    "url": "/_nuxt/223c0a5a35b256c68f8d.js",
+    "revision": "af9c334db86b026144d3fef483d171e5"
   },
   {
-    "url": "/_nuxt/3767cac2e14b7ab5043a.js",
-    "revision": "510565c88eaf9d895e1a33e855c71e7a"
+    "url": "/_nuxt/32bbfe1142848cb32a0e.js",
+    "revision": "652d457818dcf8b4e216c29bf2c0b0f4"
   },
   {
-    "url": "/_nuxt/5d63133676d57e6e7135.js",
-    "revision": "4887f261e604b469afe2742d62444bfd"
+    "url": "/_nuxt/4755ab475ab59efbcb3c.js",
+    "revision": "725a2f5b76585b62c0c0f96ca8dca537"
   },
   {
-    "url": "/_nuxt/5eb54f3bf8b50f6c4437.js",
-    "revision": "4c270981ba8c09249e449c09b052af64"
+    "url": "/_nuxt/5e235224287ceab4e4f8.js",
+    "revision": "6dc735e05c259c4abaa3f177a754584a"
   },
   {
-    "url": "/_nuxt/6792ef15f7ebdbd8f7a9.js",
-    "revision": "44f7310a8f5e13cbb211ef670e38d96a"
+    "url": "/_nuxt/70dd31ab3fd0a86f31f6.js",
+    "revision": "6bb244083d35a359eba557baf77b989c"
   },
   {
-    "url": "/_nuxt/bbc05bad65e772888400.js",
-    "revision": "a3d86a58369200660102ad2c8c07a1e8"
+    "url": "/_nuxt/cc0fd991a927a3fbe044.js",
+    "revision": "edbeff43ebeabd285363219d6b1a507b"
   }
 ], {
   "cacheId": "my-nuxt-app",
